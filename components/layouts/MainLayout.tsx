9e10d74973f6c281f4c5b424519fef6af112a885
Nico Yeager <nicoyeager@gmail.com>
2022-06-10 01:17:21 -0400

    added all code.

---
+++ components/layouts/MainLayout.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+
+
+
+const mainLayout: React.FC = props => {
+  return (
+    <div>mainLayout</div>
+  )
+}
+
+mainLayout.propTypes = {}
+
+export default mainLayout
